09c9c95464b86d85b07dac1ee4be0052deb51d80
Panagiotis <35231321+arstotzkan@users.noreply.github.com>
2022-12-11 18:50:54 +0200

    functions to find instrument codes and sound paths

---
+++ maps.js
@@ -0,0 +1,44 @@
+let valsToInstruments = [
+    "AC",
+    "BD",
+    "SD",
+    "LT",
+    "MT",
+    "HT",
+    "RS",
+    "CP",
+    "CB",
+    "CY",
+    "OH",
+    "CH"
+]
+
+let instrumentsToPaths = {
+    "AC": "",
+    "BD": "TR808WAV/BD/BD1000.WAV",
+    "SD": "TR808WAV/SD/SD1000.WAV",
+    "LT": "TR808WAV/LT/LT10.WAV",
+    "MT": "TR808WAV/MT/MT10.WAV",
+    "HT": "TR808WAV/HT/HT10.WAV",
+    "RS": "TR808WAV/RS/RS.WAV",
+    "CP": "TR808WAV/CP/CP.WAV",
+    "CB": "TR808WAV/CB/CB.WAV",
+    "CY": "TR808WAV/CY/CY1000.WAV",
+    "OH": "TR808WAV/OH/OH10.WAV",
+    "CH": "TR808WAV/CH/CH.WAV",
+    //MA, MC, LC, HC, CL UNUSED AS OF NOW
+    "MA": "TR808WAV/MA/MA.WAV",
+    "LC": "TR808WAV/LC/LC10.WAV",
+    "MC": "TR808WAV/MC/MC10.WAV",
+    "HC": "TR808WAV/HC/HC10.WAV",
+    "CL": "TR808WAV/CL/CL.WAV",
+}
+
+function getInstrument(){
+    let inputVal = document.getElementById("instrument-select").value;
+    return valsToInstruments[inputVal];
+}
+
+function getAudioPath(instrument){
+    return instrumentsToPaths[instrument]
+}
