20dbb45936da88c4ab299f844280de66cc8a33e5
Panagiotis <35231321+arstotzkan@users.noreply.github.com>
2022-12-25 20:44:26 +0200

    now can convert from interior value to input value

--- drumConfig.js
+++ drumConfig.js
@@ -17,16 +17,19 @@ const MACHINE = {
     toggle_light_timeout: undefined, //this exists only to be able to kill the MACHINE
     killMachine: function(){ clearTimeout(this.toggle_light_timeout)}, //no need to write anything here
 
-    tempo: 910,
+    tempo: _calcTempo(1),
     setTempo: function(value) { this.tempo =  _calcTempo(value) },
+    tempoToInputValue: function(){return _tempoToInputValue(this.tempo)},
 
-    masterVolume: 0.5,
+    masterVolume: _calcMasterVolume(5),
     setMasterVolume: function(value) {this.masterVolume = _calcMasterVolume(value)},
+    masterVolumeToInputValue: function(){return _masterVolumeToInputValue(this.masterVolume)},
 
-    basicVariation: "A",
+    basicVariation: _getBasicVariation(1),
     setBasicVariation: function(value) {this.basicVariation = _getBasicVariation(value)},
+    basicVariationToInputValue: function(){return _basicVariationToInputValue(this.basicVariation)},
 
-    subBeat: "A",
+    subBeat: _getBasicVariation(1),
     changeSubBeat: function() {this.subBeat = _getOtherSubBeat(this.subBeat) },
 
     currentButtonIndex : 0,
@@ -37,12 +40,31 @@ function _calcTempo(value){
     return 1000 - ( 90 * value);
 }
 
+function _tempoToInputValue(tempo){
+    return (1000 - tempo ) / 90
+}
+
 function _calcMasterVolume(value){
     return value / 10;
 }
 
+function _masterVolumeToInputValue(value){
+    return value * 10;
+}
+
 function _getBasicVariation(value){
     return ["A", "AB", "B"][value - 1];
+}
+
+function _basicVariationToInputValue(value){
+    switch(value){
+        case "A":
+            return 0;
+        case "AB":
+            return 1;
+        case "B":
+            return 2;
+    }
 }
 
 function _getOtherSubBeat(currentSubBeat){
