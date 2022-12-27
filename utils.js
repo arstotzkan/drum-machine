139604e102c1779ace8f391e5535860a8e85eea0
Panagiotis <35231321+arstotzkan@users.noreply.github.com>
2022-12-27 18:39:36 +0200

    true reset func

--- utils.js
+++ utils.js
@@ -4,3 +4,19 @@ function rhythmCheckboxesOnInstrumentChange(newInstrumentValue){
         cb.checked = (instrumentsInButtons[MACHINE.subBeat][position].includes(newInstrumentValue));
     }
 }
+
+function drumMachineReset(){
+    for (let attr in instrumentsInButtons){//maybe we could move this to the reset button once that is made
+        for (let inst in instrumentsInButtons[attr]){
+            instrumentsInButtons[attr][inst] = [];
+        }
+    }
+
+    INSTRUMENT_CONTROLLER.currentInstrumentIndex = 1;
+    MACHINE.reset();
+    INSTRUMENT_CONTROLLER.reset();
+}
+
+function deep_copy(obj, msg){ //testing shit
+    console.log(JSON.parse(JSON.stringify(obj)), msg)
+}
