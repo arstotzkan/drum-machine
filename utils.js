99a41885e5eac54f66ef1db2c1320710df858275
Panagiotis <35231321+arstotzkan@users.noreply.github.com>
2023-01-05 21:14:43 +0200

    fixed bug w/ instrument change

--- utils.js
+++ utils.js
@@ -1,7 +1,8 @@
 function rhythmCheckboxesOnInstrumentChange(newInstrumentValue){
     for (let cb of document.querySelectorAll(".rhythm-checkbox")){
         let position = cb.getAttribute("data-position")
-        cb.checked = (instrumentsInButtons[MACHINE.subBeat][position].includes(newInstrumentValue));
+                
+        cb.checked = (instrumentsInButtons[MACHINE.subBeat][position].includes(Number(newInstrumentValue)));
     }
 }
 
